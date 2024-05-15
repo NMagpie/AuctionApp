3c3fa27c0b8478a308c66aa12192a51a3f0761a6
NMagpie <sorochin.work@gmail.com>
2024-05-15 15:59:22 +0300

    ADDED TRANSACTION MIDDLEWARE

---
+++ Presentation/Middlewares/TransactionMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Persistance;
+
+namespace Presentation.Middlewares;
+public class TransactionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TransactionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext, AuctionAppDbContext dbContext)
+    {
+        if (httpContext.Request.Method == HttpMethod.Get.Method)
+        {
+            await _next(httpContext);
+            return;
+        }
+
+        using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+        await _next(httpContext);
+
+        await dbContext.Database.CommitTransactionAsync();
+    }
+}
